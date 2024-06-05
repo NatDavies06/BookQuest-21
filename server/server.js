daebb1072b302c04e9c549446d6be3baceca6f5d
NatDavies06 <153016845+NatDavies06@users.noreply.github.com>
2024-06-04 21:52:13 -0400

    Update server.js
    added apollo middleware and connection

--- server/server.js
+++ server/server.js
@@ -5,6 +5,18 @@ const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+
+  app.use(express.urlencoded({ extended: true }));
+  // app.use(express.json());
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
